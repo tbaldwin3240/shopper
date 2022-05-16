2ea11efb9941c4dad53fcea0b2975fad728434d4
COR Coding <Tbaldwin3240@gmail.com>
2022-05-15 20:43:14 -0400

    Made changes to the code

--- src/App.js
+++ src/App.js
@@ -1,57 +1,50 @@
-import React, {useState} from 'react';
+import React, {useState} from 'react'; 
+import Nav from './Nav'; 
 import './App.css';
-import Nav from './Nav'
-import ItemPage from './ItemPage';
-import{items} from './static-data'
-const App = () => {
-  const [activeTab, setActiveTab] = useState('items');
-    return(
-      <div className='App'>
-        <Nav
-        activeTab={activeTab}
-        onTabChange={setActiveTab}
-        />
-        <main className="App-content">
-          <Content tab={activeTab}/>
-        </main>
-      </div>
-    );
-};
-const Content = ({tab}) => {
-  switch (tab) {
-    case 'items' :
-      return <ItemPage items={items}/>
-    case 'cart' :
-      return <span>the cart</span>
-    default:
-      break;
+import { items } from './static-data';
+import ItemPage from './ItemPage'; 
+import CartPage from './CartPage';
+
+const summarizeCart = (cart) => {
+  const groupItems = cart.reduce((summary, item) => {
+  summary[item.id] = summary[item.id] || {
+  ...item,
+  count: 0
   }
-}
-export default App;
+  summary[item.id].count++;
+  return summary;
+  }, {});
+  return Object.values(groupItems);
+  };
 
+ const App = () => { 
+   const [activeTab, setActiveTab] = useState('items');
+   const [cart,setCart] = useState([]);
+    const addToCart = (item) => {
+      setCart([...cart,item]);
+    };
 
+return ( 
+<div className="App"> 
+<Nav activeTab ={activeTab} onTabChange ={setActiveTab}/> 
+<main className="App-content"> 
+<Content tab = {activeTab} onAddToCart = {addToCart} cart = {summarizeCart(cart)}/>
+</main> 
+</div> 
 
+);
+ };
+ 
+const Content = ({tab, onAddToCart, cart}) => {
+  switch (tab) {
+    case 'items':
+      return <ItemPage items= {items} onAddToCart={onAddToCart}/>;
+      case 'cart':
+      return <CartPage items={cart}/>
+      default:
+        break;
+  }
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
+export default App; 
--- src/ItemPage.js
+++ src/ItemPage.js
@@ -1,19 +1,22 @@
-import React from 'react'; 
-import PropTypes from 'prop-types'; 
-import './ItemPage.css'; 
- function ItemPage({items}) { 
-return ( 
-<ul className="ItemPage-items"> 
-{items.map(item => 
-<li key={item.id} className="ItemPage-item"> 
-{item.name} 
-</li>)} 
-</ul> 
-); 
-} 
- 
-ItemPage.propTypes = { 
-items: PropTypes.array.isRequired 
-}; 
- 
-export default ItemPage;  
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ItemPage.css';
+import Item from './Item';
+function ItemPage({items, onAddToCart}) {
+    return (
+    <ul className="ItemPage-items">
+    {items.map(item =>
+    <li key={item.id} className="ItemPage-item">
+    <Item item={item} onAddToCart={() => onAddToCart(item)}/>
+    </li>)}
+    </ul>
+    );
+    }
+    ItemPage.propTypes = {
+        items: PropTypes.array.isRequired
+        };
+     export default ItemPage;
+
+
+
+
